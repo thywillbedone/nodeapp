ab0256bd6fd75e51c0e4e3c7818530087878c417
Damian Ferro <dfmartinez8909@gmail.com>
2022-03-11 20:49:36 +0100

    adapt UT

--- src/app.test.js
+++ src/app.test.js
@@ -5,7 +5,7 @@ const app = require("./app");
 describe("GET /", () => {
     //navigate to root and check the the response is "Hello World!"
     it('responds with "Hello World!"', (done) => { 
-        request(app).get('/').expect('Hello World!', done);
+        request(app).get('/').expect('Hello World from Express!', done);
     });
 });
 
